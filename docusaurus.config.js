f41705733ebf2220fe3874afbc5587c2597a06c8
jasonwu513 <jasonwu513@gmail.com>
2023-08-16 22:56:22 +0800

    ph-pages config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'jasonwu513', // Usually your GitHub org/user name.
+  projectName: 'goldfish-memory', // Usually your repo name.
 
   markdown: {
     mermaid: true,
